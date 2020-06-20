46c92ebff1d598f38b375cd69bd66be66ac9cbf2
unknown <haritrichy@yahoo.com>
2020-06-20 18:11:33 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='db_proj1',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Databricks Labs CICD Templates Sample Project',
+    author='hari',
+    license='',
+)
